659de5fb8bf69eef74d42931240ff9695a072b0e
rzepkka <rzepkajulia@gmail.com>
2022-01-24 17:05:33 +0100

    Update home_app.py

--- home_app.py
+++ home_app.py
@@ -23,5 +23,6 @@ def load_image(image_file):
 def run_home_app():
 	st.image(load_image('images/home_2.png'), use_column_width  = True, )
 	st.subheader("Welcome to Alzheimer's Detection Application!")
+	st.write("Explainable AI provides transparency in model decisions and helps adapting ML models in contextual usage as professionals are provided with transparency by retracing the decision made. This enables to critically scrutinize the decision and builds trust.")
+	st.write("The machine learning (ML) model classifies MRI images in four stages of Alzheimer. The aim of our application is to include an explainable component to show professionals on which are of the image the classifier made their decision upon. ")
 	st.subheader("")
-	# st.subheader("? Maybe add some intro here ?")
